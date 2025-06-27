74fbe71e7e4771c5f8208b87c0b6d89a65199ffa
xllvrr <dan@dtsa.email>
2025-06-27 17:17:33 +0800

    Added all weeks up to Week 5

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  buildInputs = with pkgs; [
+    stdenv.cc.cc
+    libuv
+    zlib
+    graphviz
+  ];
+in {
+  env = {LD_LIBRARY_PATH = "${with pkgs; lib.makeLibraryPath buildInputs}";};
+
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  scripts.hello.exec = "uv run python hello.py";
+
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+    hello
+  '';
+}
